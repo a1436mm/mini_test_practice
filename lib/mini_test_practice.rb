af9745bf4a5d53e7d2ad4307b6f4b1bbf9dbf5cc
a1436mm <a1436mm@aiit.ac.jp>
2014-11-25 11:36:59 +0000

    LastVersion

--- lib/mini_test_practice.rb
+++ lib/mini_test_practice.rb
@@ -3,57 +3,57 @@ require "mini_test_practice/version"
 module MiniTestPractice
 class MyClass
       def odd?(value)
-      if value % 2 == 0 then
-       return false
-	end
-      return true
+          if value % 2 == 0 then
+             return false
+          end
+          return true
       end
 
       def check_number?(value)
-      if value.to_i > 999 then
-         if value % 2 == 0 then
-            return true
-         end
-      return false 
-      end
-      return false 
+          if value.to_i > 999 then
+              if value % 2 == 0 then
+                 return true
+              end
+              return false 
+          end
+          return false 
       end
 
       def enough_length?(value)
-      if value.length > 2 and value.length < 9 then
-       return true
-      end
-      return false
+          if value.length > 2 and value.length < 9 then
+              return true
+          end
+          return false
       end
 
       def devide(a,b)
-      if b == 0 then
-      throw ZeroDivisionError
-      end 
-  
-      value = a.to_f/b.to_f
-      return value 
+          if b == 0 then
+              throw ZeroDivisionError
+          end 
+          value = a.to_f/b.to_f
+          return value 
       end
 
       def fizz_buzz(value)
-      if value == 0 then return "" end
-      if value % 15 == 0 then
-          return 'FizzBuzz'
-      else
-          if value % 5 == 0 then
-              return 'Buzz'
+          if value == 0 then return "" end
+              if value % 15 == 0 then
+                  return 'FizzBuzz'
           else
-             if value % 3 == 0 then
-                 return 'Fizz'
-             else
-                 return ''
-             end 
+              if value % 5 == 0 then
+                  return 'Buzz'
+              else
+                 if value % 3 == 0 then
+                     return 'Fizz'
+                 else
+                     return ''
+                 end 
+              end
           end
-      end
       end
 
       def Hello
-      print "Hello"
+          print "Hello"
       end
 end
 end
+
--- test/test_mini_test_practice.rb
+++ test/test_mini_test_practice.rb
@@ -4,9 +4,11 @@ class TestMiniTestPractice < MiniTest::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::MiniTestPractice::VERSION
   end
+
   def setup
       @myClass = MiniTestPractice::MyClass.new
   end
+
   def test_odd?
     assert_equal false, @myClass.odd?(0)
     assert_equal true, @myClass.odd?(1)
@@ -14,35 +16,35 @@ class TestMiniTestPractice < MiniTest::Unit::TestCase
   end
   
   def test_number?
-  assert_equal true, @myClass.check_number?(1234)
-  assert_equal false, @myClass.check_number?(999)
-  assert_equal false, @myClass.check_number?(0124)
-  assert_equal false, @myClass.check_number?(1235)
-  assert_equal false, @myClass.check_number?('2i35i')
+    assert_equal true, @myClass.check_number?(1234)
+    assert_equal false, @myClass.check_number?(999)
+    assert_equal false, @myClass.check_number?(0124)
+    assert_equal false, @myClass.check_number?(1235)
+    assert_equal false, @myClass.check_number?('2i35i')
   end
 
   def test_enough_length?
-  assert_equal true, @myClass.enough_length?('jdhsj')
-  assert_equal false, @myClass.enough_length?('1k')
-  assert_equal true, @myClass.enough_length?('12345678')
-  assert_equal false, @myClass.enough_length?('12345678910')
+    assert_equal true, @myClass.enough_length?('jdhsj')
+    assert_equal false, @myClass.enough_length?('1k')
+    assert_equal true, @myClass.enough_length?('12345678')
+    assert_equal false, @myClass.enough_length?('12345678910')
   end
 
   def test_devide
-  assert_equal 10 ,@myClass.devide(20,2)
-  assert_equal 2.5  ,@myClass.devide(10,4)
-  assert_throws(ZeroDivisionError){ @myClass.devide(10,0)}
+    assert_equal 10 ,@myClass.devide(20,2)
+    assert_equal 2.5  ,@myClass.devide(10,4)
+    assert_throws(ZeroDivisionError){ @myClass.devide(10,0)}
   end
 
   def test_fizz_buzz
-  assert_equal 'Fizz' ,@myClass.fizz_buzz(3)
-  assert_equal 'Buzz' ,@myClass.fizz_buzz(5)
-  assert_equal 'FizzBuzz' ,@myClass.fizz_buzz(15)
-  assert_equal '', @myClass.fizz_buzz(0)
-  assert_equal '' ,@myClass.fizz_buzz(2)
+    assert_equal 'Fizz' ,@myClass.fizz_buzz(3)
+    assert_equal 'Buzz' ,@myClass.fizz_buzz(5)
+    assert_equal 'FizzBuzz' ,@myClass.fizz_buzz(15)
+    assert_equal '', @myClass.fizz_buzz(0)
+    assert_equal '' ,@myClass.fizz_buzz(2)
   end
 
   def test_Hello
-  assert_equal  'Hello',capture_io{@myClass.Hello}[0]
+    assert_equal  'Hello',capture_io{@myClass.Hello}[0]
   end
 end
